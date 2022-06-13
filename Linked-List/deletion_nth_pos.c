db003273ac216895bc58a59b49d1ad6276b642f5
Rishabh S Chauhan <rishabh007singh@gmail.com>
2022-06-13 22:13:42 +0530

    Deletion of a node at nth position
    * Function named Delete was created
    * Program ran successfully
    * Logic for Delete function explained with the help of comments

--- Linked-List/deletion_nth_pos.c
+++ Linked-List/deletion_nth_pos.c
@@ -16,7 +16,7 @@ void Insert(int num)
     struct Node *temp = (struct Node *)malloc(sizeof(struct Node *));
     temp->data = num;
     temp->next = NULL;
-    
+
     if (head == NULL)
     {
         temp->next = head;
@@ -24,29 +24,42 @@ void Insert(int num)
         return;
     }
 
-    struct Node *traverse = head; 
+    struct Node *traverse = head;
 
     while (traverse->next != NULL)
     {
         traverse = traverse->next;
     }
     traverse->next = temp; // Last node is linked with the incoming node
+}
+
+void Delete(int pos)
+{
+    struct Node *temp1 = head;
+    if (pos == 1)
+    {
+        head = temp1->next;
+        free(temp1);
+        return;
+    }
+    for (int i = 0; i < pos-2; i++)
+    {
+        temp1 = temp1->next;
+    }
+    // temp1 points to (pos-1)th node
+    struct Node* temp2 = temp1->next; // temp2 points to the node at position pos
+    temp1->next = temp2->next;// (pos-1)th node is linked with (pos+1)th node
+    free(temp2); //delete temp2
     
 }
 
-void delete(int pos)
-{
-
-}
-
-
 void Print()
 {
-    struct Node *temp2 = head;
-    while (temp2 != NULL) 
+    struct Node *temp = head;
+    while (temp != NULL)
     {
-        printf("%d ", temp2->data);
-        temp2 = temp2->next;
+        printf("%d ", temp->data);
+        temp = temp->next;
     }
 }
 
@@ -56,7 +69,7 @@ int main()
     Insert(2);
     Insert(4);
     Insert(6);
-    Insert(5); //List would look like : 2,4,6,5
+    Insert(5); // List would look like : 2,4,6,5
     printf("The Linked List is : ");
     Print();
     printf("\n");
@@ -67,4 +80,4 @@ int main()
     printf("The Linked List after deletion is : ");
     Print();
     printf("\n");
-}    
+}
