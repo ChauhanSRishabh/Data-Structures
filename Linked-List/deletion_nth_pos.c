3fb4df1b02a9c9bb87aaa28c043bcedc7658921f
Rishabh S Chauhan <rishabh007singh@gmail.com>
2022-06-13 16:00:23 +0530

    Created a function named Insert() * Nodes are added to the end of the LL

--- Linked-List/deletion_nth_pos.c
+++ Linked-List/deletion_nth_pos.c
@@ -3,9 +3,34 @@
 
 // LINKED LIST : DELETE A NODE AT nth POSITION
 
-struct node
+struct Node
 {
     int data;
-    struct node *next;
+    struct Node *next;
 };
+
+struct Node *head;
+
+void Insert(int num)
+{
+    struct Node *temp = (struct Node *)malloc(sizeof(struct Node *));
+    temp->data = num;
+    temp->next = NULL;
+    
+    if (head == NULL)
+    {
+        temp->next = head;
+        head = temp;
+        return;
+    }
+
+    struct Node *traverse = head; 
+
+    while (traverse->next != NULL)
+    {
+        traverse = traverse->next;
+    }
+    traverse->next = temp; // Last node is linked with the incoming node
+    
+}
 
