bd6d786dee1abd6d8efdc2e6859244e6c1f76d79
Rishabh S Chauhan <rishabh007singh@gmail.com>
2022-06-16 13:39:51 +0530

    Added a function named ReversePrint to display the contents of DLL in reverse order

--- Doubly-Linked-List/implementation.c
+++ Doubly-Linked-List/implementation.c
@@ -42,6 +42,25 @@ void Print()
     printf("\n");
 }
 
+void ReversePrint()
+{
+    struct Node* temp = head;
+    if (temp == NULL) return; // empty list, exit
+    // Going to last Node
+    while(temp->next != NULL) 
+    {
+        temp = temp->next;
+    }
+    // Traversing backward using prev pointer
+    printf( "Reverse: ") ;
+    while(temp != NULL)
+    {
+        printf("%d ",temp-›data);
+        temp = temp-›prev;
+    }
+    printf("\n");
+}
+
 void InsertAtHead(int num)
 {
     struct Node* newNode = GetNewNode(num); //this newNode is local to InsertAtHead
