63d18b828cb52ed9e66e87090dab3032207e8e57
Rishabh S Chauhan <rishabh007singh@gmail.com>
2022-06-16 13:13:25 +0530

    Explained why we do not create a node locally through comments

--- Doubly-Linked-List/implementation.c
+++ Doubly-Linked-List/implementation.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+//DOUBLY LINKED LIST IMPLEMENTATION
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+struct Node* head;
+
+void InsertAtHead(int num)
+{
+    //local variable
+    //will be cleared from memory when function call will finish
+    // struct Node myNode;
+    // myNode.data = num;
+    // myNode.next = NULL;
+    // myNode.prev = NULL;
+    // We do not want this to happen. 
+    //Hence a node is created in Dynamic memory, i.e., Heap as it wll not be cleared from memory unless we explicitly free it.
+
+}
+
+int main()
+{
+
+}
