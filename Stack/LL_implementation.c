d00046a096db868a4bfe15cf8f468f7a171f21be
Rishabh S Chauhan <rishabh007singh@gmail.com>
2022-06-17 19:49:03 +0530

    Successfully implemented Push function on a Stack using LL

--- Stack/LL_implementation.c
+++ Stack/LL_implementation.c
@@ -2,4 +2,49 @@
 #include <stdlib.h>
 
 // STACK - LINKED LIST BASED IMPLEMENTATION
+// to implement a stack using LL, we basically have to insert and delete elements at the top/head
 
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+struct Node* top = NULL;
+
+struct Node* GetNewNode(int x)
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = x;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void Push(int x)
+{
+    struct Node* newNode = GetNewNode(x);
+    newNode->next = top;
+    top = newNode;
+}
+
+void Print()
+{
+    struct Node *temp = top;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+
+int main()
+{
+    Push(2);
+    Push(3);
+    Push(5);
+    Push(7);
+    printf("Stack : ");
+    Print();
+    printf("\n");
+}
