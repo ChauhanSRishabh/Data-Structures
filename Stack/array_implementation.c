1860326dce45f5b1d31feabbc67b31e1097809ce
Rishabh S Chauhan <rishabh007singh@gmail.com>
2022-06-17 12:35:27 +0530

    Push function for array implementation of stack written

--- Stack/array_implementation.c
+++ Stack/array_implementation.c
@@ -1,2 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+//STACK - ARRAY BASED IMPLEMENTATION
+
+#define MAX_SIZE 101
+
+int A[MAX_SIZE];
+int top = -1;
+
+void Push(int x)
+{
+    if(top == MAX_SIZE - 1)
+    {
+        printf("Stack Overflow\n");
+        return;
+    }
+    A[++top] = x;
+}
+
+
