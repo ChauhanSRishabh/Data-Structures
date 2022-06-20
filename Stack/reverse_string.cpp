c61dfe82effac1ba071480c979bdfe8ebb8cb1b1
Rishabh S Chauhan <rishabh007singh@gmail.com>
2022-06-20 19:22:19 +0530

    Created an object of class stack using STL

--- Stack/reverse_string.cpp
+++ Stack/reverse_string.cpp
@@ -1,22 +1,14 @@
-#include<iostream>
-#include<stack> //stack from STL
+#include <iostream>
+#include <stack> //stack from STL
+// when we use the above method, we have a 'stack' class available to us
+// To create an object of this class, we write stack<data_type> name/identifier
 
 using namespace std;
 
-//STRING REVERSAL USING STACK
+// STRING REVERSAL USING STACK
 
-/* 
-You can write your own stack in such a way
-class Stack
+void Reverse(char C[], int n) // We could have written char* C instead of char C[]
 {
-    private:
-        char A[101];
-        int top;
-    public:
-        void Push(int x);
-        void Pop();
-        int Top();
-        bool isEmpty();
-};
-OR use Language Libraries
-*/
+    stack<char> S; // Created a stack of characters named S using STL
+
+}
