493680bbb7874ea99ebb2f6d0cd0cf966933cd5e
Rishabh S Chauhan <rishabh007singh@gmail.com>
2022-06-20 19:13:52 +0530

    Showed how to write your own stack or use Language Libraries instead

---
+++ Stack/reverse_string.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stack> //stack from STL
+
+using namespace std;
+
+//STRING REVERSAL USING STACK
+
+/* 
+You can write your own stack in such a way
+class Stack
+{
+    private:
+        char A[101];
+        int top;
+    public:
+        void Push(int x);
+        void Pop();
+        int Top();
+        bool isEmpty();
+};
+OR use Language Libraries
+*/
