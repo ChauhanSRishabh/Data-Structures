a467326953b69729a8b07e5e6add8ddb6f45b317
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-06-21 20:31:39 +0530

    Added functions to create and display LL

--- Stack/reverse_linked_list.cpp
+++ Stack/reverse_linked_list.cpp
@@ -15,3 +15,48 @@ Time Complexity = O(n)
 Space Complexity = O(1)
 */
 
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node* head;
+
+void Insert(int num)
+{
+    struct Node *temp = (struct Node *)malloc(sizeof(struct Node *));
+    temp->data = num;
+    temp->next = NULL;
+
+    if (head == NULL)
+    {
+        temp->next = head;
+        head = temp;
+        return;
+    }
+
+    struct Node *traverse = head;
+
+    while (traverse->next != NULL)
+    {
+        traverse = traverse->next;
+    }
+    traverse->next = temp; // Last node is linked with the incoming node
+}
+
+void Print()
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+
+//USING EXPLICIT STACK
+
+//pushing references/pointers to the nodes in the stack
+
