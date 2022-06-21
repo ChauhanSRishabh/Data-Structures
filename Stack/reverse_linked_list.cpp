ccb8d96849a42d14a234dfc62d795146275d247b
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-06-21 20:23:15 +0530

    Time Complexity of recursive and iterative solutions included

---
+++ Stack/reverse_linked_list.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+
+//USING STACK TO REVERSE A LL
+
+/*
+REVERSING LL
+
+Recursive Solution (Here we are using IMPLICIT STACK, think of the diagram that will be a result of recursive function calls inside the stack)
+Time Complexity = O(n)
+Space Complexity = O(n)
+
+Iterative Solution
+Time Complexity = O(n)
+Space Complexity = O(1)
+*/
+
