3660b1618a45cca07469087086ec3aab19c17768
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-06-22 17:49:53 +0530

    Creating a program to evaluate expressions *Infix, Prefix and Postfix Expressions *Infix Expression explained

---
+++ Stack/evaluate_expression.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stack>
+
+using  namespace std;
+
+//EVALUATION OF EXPRESSIONS
+
+/*
+INFIX : <operand> <operator> <operand>
+Operator is placed in between operands
+Operand : object on which operation is performed
+does not need to be a constant/variable, it can be an expression itself
+(p+q)*(r+s)
+*/
