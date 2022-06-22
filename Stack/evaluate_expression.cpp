d9051e2deef62987d7ddcc01a51193263d54adcb
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-06-22 18:12:14 +0530

    Prefix notation defined and explained

--- Stack/evaluate_expression.cpp
+++ Stack/evaluate_expression.cpp
@@ -17,4 +17,17 @@ Order of Operation:
 2) Exponents : ^ ; right to left/Right Associative ; => 2^3^2 = 2^9 = 512
 3) Division and Multiplication : left to right/Left Associative ; If both are present, evaluate leftmost first as precedence is same. Also called Operator Associativity
 4) Addition and Subtraction : left to right ; If both are present, evaluate leftmost first as precedence is same
+
+Even though infix is most common way of writing expressions, it's not easy to parse and evaluate an infix exression without ambiguity. Hence, mathematicians came up with 2 other ways of writing expressions:
+1) POSTFIX
+2) PREFIX
+
+PREFIX : <operator> <operand> <operand>
+Also called Polish notation
+Operator is placed before operands
+2+3 in Infix will be written as +23 in Prefix
+a+b*c will be +a*bc 
+Just like Infix, Operand does not need to be a constant/variable, it can be an expression itself
+In Prefix form, an operand can only be associated with 1 operator, unlike Infix
+
 */
