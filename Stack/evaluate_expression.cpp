9ac9314df01a7e4df20e0ee18b34cc862ff55cdc
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-06-23 10:01:12 +0530

    Algorithm for Infix to Postfix discussed

--- Stack/evaluate_expression.cpp
+++ Stack/evaluate_expression.cpp
@@ -36,4 +36,47 @@ Operator is placed after the operands
 2+3 in Infix will be written as 23+ in Prefix
 a+b*c will be abc*+ 
 
+CONVERSIONS :
+PREFIX : a + b * c => a + (b*c) => a + (*bc) => +a (*bc) => +a*bc
+POSTFIX : a + b * c => a + (b*c) => a + (bc*) => a(bc*)+ => abc*+
+
+Infix : most human-readable
+Prefix and Postfix : good for machines
+*/
+
+/*
+
+INFIX TO POSTFIX
+A*B+C*D-E
+(A*B)+(C*D)-E
+(AB*)+(CD*)-E
+{(AB*)(CD*)+}-E
+{(AB*)(CD*)+}E-
+AB*CD*+E-
+
+How is evaluation done?
+AB*CD*+E-
+A=2 B=3 C=5 D=4 E=9
+23*54*+9-
+In Postfix expression, operands of an operator lie to it's left
+<op1> <op2> <operator>
+=> 6 54* +9-
+=> 6 20 + 9-
+=> 26 9-
+=> 17
+
+ALGORITHM
+Parse from Left to Right
+get a token of either operator or operand
+List : 2 3 
+Now * operator, pop preceding 2 and perform operation
+List : 6 5 4
+Now * operator, pop preceding 2 and perform operation
+List : 6 20
+and so on..........
+
+What comes in last, goes out first.
+Our List is nothing but a Stack. Draw the logical implementation.
+
+This is an efficient algo as we are doing just one pass on the expression
 */
