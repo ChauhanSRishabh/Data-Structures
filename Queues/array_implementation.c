db2f4f1d11d0ac2757a2eef15c86263cee6131e8
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-06-25 22:53:09 +0530

    Reamed implementation.c to array_implementation.c Function ot check whether queue is empty or not written

---
+++ Queues/array_implementation.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+// QUEUES - ARRAY BASED IMPLEMENTATION
+
+#define MAX_SIZE 10
+
+int A[MAX_SIZE];
+int front = -1, rear = -1;
+
+bool isEmpty()
+{
+    if(front = -1 && rear = -1)
+        return true;
+    else
+        return false;
+}
+
