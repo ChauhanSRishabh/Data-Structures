d65c08f975822fcf59080e87dcbec4c45ea927c0
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-07-14 14:07:52 +0530

    Finding min and max element in a BST using Iteration FindMin() function prints the minimum value FindMax() function gives the maximum value

--- Trees/iterative_min_max_bst.c
+++ Trees/iterative_min_max_bst.c
@@ -35,6 +35,8 @@ struct BstNode *Insert(struct BstNode *root, int data)
 
 void FindMin(struct BstNode *root)
 {
+    // We are not assigning root to a temporary variable as root being a local variable of main() is passed on to the FindMin() function and changes made here won't affect root unlike the case where root would have been a global variable
+
     if (root == NULL)
     {
         printf("Error : No nodes in BST\n");
@@ -42,13 +44,29 @@ void FindMin(struct BstNode *root)
     }
 
     // We known that minimum element in a BST is the leftmost leaf node for Left subtree
-    // We are not assigning root to a temporary variable as root being a local variable of main() is passed on to the FindMin() function and changes made here won't affect root unlike the case where root would have been a global variable
 
     // Using Iteration to reach the leftmost leaf node in the BST
     while (root->left != NULL)
         root = root->left;
 
-    printf("Minimum element is : %d", root->data);
+    printf("Minimum element is : %d\n", root->data);
+}
+
+void FindMax(struct BstNode *root)
+{
+    if (root == NULL)
+    {
+        printf("Error : No nodes in BST\n");
+        return;
+    }
+
+    // We known that maximum element in a BST is the rightmost leaf node for the right subtree
+
+    // Using Iteration to reach the rightmost leaf node in the BST
+    while (root->right != NULL)
+        root = root->right;
+
+    printf("Maximum element is : %d\n", root->data);
 }
 
 int main()
@@ -57,12 +75,13 @@ int main()
 
     root = Insert(root, 15);
     root = Insert(root, 10);
-    root = Insert(root, 2);
-    root = Insert(root, 4);
+    root = Insert(root, 99);
+    root = Insert(root, 98);
     root = Insert(root, 1);
     root = Insert(root, 12);
 
     FindMin(root);
+    FindMax(root);
 
     return 0;
 }
