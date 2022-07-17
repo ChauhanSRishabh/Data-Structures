76207ff67605abd1163008b02daa4a66a0b73d98
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-07-17 14:38:45 +0530

    Breadth First Search implemented * Formatting improved

--- Trees/breadth_first_search.cpp
+++ Trees/breadth_first_search.cpp
@@ -35,9 +35,9 @@ void LevelOrderTraversal(Node *root)
         Node *current = Q.front();    // assigning the pointer to node at front of the queue to a temporary pointer
         cout << current->data << " "; // display content of the node
         if (current->left != NULL)    // check to see if it has a left child
-            Q.push(current->left);       // push the left child into the queue
+            Q.push(current->left);    // push the left child into the queue
         if (current->right != NULL)   // check to see if it has a right child
-            Q.push(current->right);      // push the right child into the queue
+            Q.push(current->right);   // push the right child into the queue
         Q.pop();                      // remove the element at front
     }
 }
@@ -45,17 +45,17 @@ void LevelOrderTraversal(Node *root)
 int main()
 {
     /*Code To Test the logic
-	  Creating an example tree
-	            B
-			   / \
-			  A   C
-			 / \
-			Z   D
-    
+      Creating an example tree
+                B
+               / \
+              A   C
+             / \
+            Z   D
+
     */
     Node *root = GetNewNode('B');
     root->left = GetNewNode('A');
-    root->right =GetNewNode('C');
+    root->right = GetNewNode('C');
     root->left->left = GetNewNode('Z');
     root->left->right = GetNewNode('D');
 
