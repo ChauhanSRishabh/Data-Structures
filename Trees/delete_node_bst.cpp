82ee5e3f95c359a8c4a13f85e3d38ad12ed68260
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-07-19 22:18:06 +0530

    Successfully deleted a node from a BST

--- Trees/delete_node_bst.cpp
+++ Trees/delete_node_bst.cpp
@@ -89,3 +89,51 @@ void Inorder(Node *root)
     cout << root->data << " "; // print data
     Inorder(root->right);      // visit right subtree
 }
+
+int main()
+{
+    /*Code To Test the logic
+      Creating an example tree
+                5
+               / \
+              3   10
+             / \   \
+            1   4   11
+    */
+    Node *root = NULL;
+    root = Insert(root, 5);
+    root = Insert(root, 10);
+    root = Insert(root, 3);
+    root = Insert(root, 4);
+    root = Insert(root, 1);
+    root = Insert(root, 11);
+
+    // Deleting node with value 5 and then 3
+    root = Delete(root, 5);
+    /*
+    Tree should become
+                10
+               /  \
+              3   11
+             / \
+            1   4
+    */
+    // Print Nodes in Inorder
+    cout << "Inorder after deleting node 5: ";
+    Inorder(root);
+    cout << endl;
+
+    root = Delete(root, 3);
+    /*
+    Tree should become
+                10
+               /  \
+              4   11
+             /
+            1
+    */
+
+    // Print Nodes in Inorder
+    cout << "Inorder after deleting node 3: ";
+    Inorder(root);
+}
