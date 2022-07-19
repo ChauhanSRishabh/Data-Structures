d6122d2c77f2e217be2cfa5d323d75356b4e2b42
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-07-19 21:11:38 +0530

    Dealt with the case when the node to be deleted has no child

--- Trees/delete_node_bst.cpp
+++ Trees/delete_node_bst.cpp
@@ -19,7 +19,13 @@ Node *Delete(Node *root, int data)
         root->right = Delete(root->right, data);
     else // When we find the node that is to be deleted
     {
-        
-    }
+        // Case 1 : No Child
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete root; // delete is an operator in C++ used to deallocate memory of an object in Heap. In C, we'd have used free function
+            root = NULL; // memory was deallocated but root still has it's address
+            return root; // one of the above 2 else ifs will have the correct reference/link with this
+        }
 
+    }
 }
