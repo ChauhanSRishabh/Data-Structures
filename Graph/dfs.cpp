6f78a190a9a34e598abb41eb07b7802efd8384d8
Rishabh Singh Chauhan <15028913+ChauhanSRishabh@users.noreply.github.com>
2022-09-05 20:15:29 +0530

    Depth First Search Comments improved

--- Graph/dfs.cpp
+++ Graph/dfs.cpp
@@ -18,34 +18,36 @@ using namespace std;
 
 */
 
-int visited[V] = {0, 0, 0, 0, 0, 0, 0};
-int graph[V][V] = {
-    {0, 1, 1, 1, 0, 0, 0},
-    {1, 0, 1, 0, 0, 0, 0},
-    {1, 1, 0, 1, 1, 0, 0},
-    {1, 0, 1, 0, 1, 0, 0},
-    {0, 0, 1, 1, 0, 1, 1},
-    {0, 0, 0, 0, 1, 0, 0},
-    {0, 0, 0, 0, 1, 0, 0}};
-
 // DEPTH FIRST SEARCH
-void DFS(int i)
+// Undirected and non-weighted Graph
+void DFS(int visited[], int graph[V][V], int src)
 {
-    cout << i;
-    visited[i] = 1; 
-    for (int  j = 0; j < V; j++)
+    cout << src;
+    visited[src] = 1;
+    for (int j = 0; j < V; j++)
     {
-        if(graph[i][j] == 1 && visited[j] == 0) // i the node we're at. Checking if an edge exists from i to a node that is still not visited
+        if (graph[src][j] && visited[j] == 0) // src is the node we're at. Checking if an edge exists from src to a node that is still not visited
         {
             cout << " ";
-            DFS(j);
+            DFS(visited, graph, j);
         }
     }
-    
 }
 
 int main()
 {
-    DFS(2); // pass the node you want DFS to begin from
+    int visited[V] = {0, 0, 0, 0, 0, 0, 0}; // Array to keep track of nodes that have been traversed. Initially, we haven't visited any node
+    
+    // Adjacency matrix representation of Graphs
+    int graph[V][V] = {
+        {0, 1, 1, 1, 0, 0, 0},
+        {1, 0, 1, 0, 0, 0, 0},
+        {1, 1, 0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 1, 0, 0},
+        {0, 0, 1, 1, 0, 1, 1},
+        {0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0}};
+
+    DFS(visited, graph, 2); // pass the node you want DFS to begin from along with graph and visited array
     return 0;
 }
